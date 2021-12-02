3b0293bbc1a6e519e99250e0c6c4ddb014952ac6
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-02 19:03:26 +0300

    refactor CharacterInteractionListener

--- app/src/main/java/com/example/marvelapp/ui/home/CharacterInteractionListener.kt
+++ app/src/main/java/com/example/marvelapp/ui/home/CharacterInteractionListener.kt
@@ -1,9 +1,9 @@
 package com.example.marvelapp.ui.home
 
-import com.example.marvelapp.data.local.entity.CharacterEntity
-import com.example.marvelapp.ui.base.BaseInteractionListener
 
+import com.example.marvelapp.ui.base.BaseInteractionListener
+import com.example.marvelapp.domain.models.Character
 
 interface CharacterInteractionListener  : BaseInteractionListener {
-//    fun onClickCharacter(character: Character)
+    fun onClickCharacter(character: Character)
 }
