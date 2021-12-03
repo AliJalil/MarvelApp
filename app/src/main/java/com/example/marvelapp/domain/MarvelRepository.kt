7aa7abae06f584d3a3866d8f1dfbc99c52827636
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-03 23:34:13 +0300

    add marveldatabase dependency

--- app/src/main/java/com/example/marvelapp/domain/MarvelRepository.kt
+++ app/src/main/java/com/example/marvelapp/domain/MarvelRepository.kt
@@ -10,10 +10,6 @@ interface MarvelRepository {
 
      fun getCharacters() : Flow<Resources<List<Character>?>>
      suspend fun  searchCharacters(characterName: String) : List<Character>
-
-//     fun searchCharactersX(characterName: String) : Flow<Resources<List<Character>?>>
-
      suspend fun  saveSearchCharacters(characterName: String): List<Character>
 
-//    suspend fun refreshCharacters()
 }
