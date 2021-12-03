d667bcaabf4ea9ca055ef177c5577a06fab2bfa1
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-03 23:14:47 +0300

    refactor CharacterMapper

--- app/src/main/java/com/example/marvelapp/domain/models/CharacterMapper.kt
+++ app/src/main/java/com/example/marvelapp/domain/models/CharacterMapper.kt
@@ -1,10 +1,19 @@
 package com.example.marvelapp.domain.models
 
+import com.example.marvelapp.data.local.entity.CharacterEntity
 import com.example.marvelapp.domain.mapper.Mapper
 import com.example.marvelapp.data.remote.response.CharacterDto
 
-class CharacterMapper : Mapper<CharacterDto, Character> {
-    override fun map(input: CharacterDto): Character {
+interface CharacterBaseMapper {
+    fun mapRemoteToDomain(input: CharacterDto): Character
+    fun mapRemoteToEntity(input: CharacterDto): CharacterEntity
+    fun mapEntityToDomain(input: CharacterEntity): Character
+    fun mapDomainToEntity(input: Character): CharacterEntity
+
+}
+
+class CharacterMapper : CharacterBaseMapper {
+    override fun mapRemoteToDomain(input: CharacterDto): Character {
         return Character(
             name = input.name,
             id = input.id,
@@ -13,4 +22,36 @@ class CharacterMapper : Mapper<CharacterDto, Character> {
             description = input.description
         )
     }
+
+    override fun mapRemoteToEntity(input: CharacterDto): CharacterEntity {
+        return CharacterEntity(
+            id = input.id?.toLong() ?: 0L,
+            name = input.name ?: "",
+            description = input.description ?: "",
+            modified = input.modified ?: "",
+            imageUrl = "${input.thumbnail!!.path}.${input.thumbnail!!.extension}"
+        )
+    }
+
+    override fun mapEntityToDomain(input: CharacterEntity): Character {
+        return Character(
+            name = input.name,
+            id = input.id.toInt(),
+            imageUrl = input.imageUrl,
+            modified = input.modified,
+            description = input.description
+        )
+    }
+
+    override fun mapDomainToEntity(input: Character): CharacterEntity {
+        return CharacterEntity(
+            id = input.id?.toLong() ?: 0L,
+            name = input.name ?: "",
+            description = input.description ?: "",
+            modified = input.modified ?: "",
+            imageUrl = input.imageUrl ?: "",
+        )
+    }
+
 }
+
