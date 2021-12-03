dd7e292d17997e5da91a6665195d291218d1bb43
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-03 23:34:46 +0300

    add marveldatabase dependency to MarvelRepositoryImpl

--- app/src/main/java/com/example/marvelapp/domain/MarvelRepositoryImpl.kt
+++ app/src/main/java/com/example/marvelapp/domain/MarvelRepositoryImpl.kt
@@ -8,47 +8,17 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import com.example.marvelapp.domain.models.Character
 import com.example.marvelapp.domain.models.CharacterMapper
-import retrofit2.Response
 import javax.inject.Inject
 import com.example.marvelapp.data.local.MarvelDatabase
 
 
 class MarvelRepositoryImpl @Inject constructor(
     private val apiService: MarvelApiServices,
-    private val characterMapper: CharacterMapper
+    private val characterMapper: CharacterMapper,
+    marvelDatabase: MarvelDatabase
 ) : MarvelRepository {
 
-
-//
-
-
-    private fun <T> wrapWithFlow(endPointResponse: suspend () -> Response<T>): Flow<Resources<T?>> {
-        return flow {
-            emit(Loading)
-            try {
-                val result = endPointResponse()
-                if (result.isSuccessful) {
-                    emit(Success(result.body()))
-                } else {
-                    emit(Error(throwable = Throwable(result.message())))
-                }
-            } catch (throwable: Throwable) {
-                Error(throwable)
-            }
-        }
-    }
-
-//    private val characterMapper = CharacterMapper()
-//    override fun getCharacters(): Flow<Resources<List<Character>?>> {
-//        return wrapWithFlow {
-//
-//            apiService.getCharacters().body().map {
-//                    characterDto -> characterMapper.map(characterDto)
-//            }
-//    }
-
-
-    //    private val characterMapper = CharacterMapper()
+    private val characterDao = marvelDatabase.marvelCharacterDao()
     override fun getCharacters(): Flow<Resources<List<Character>?>> {
         return flow {
             emit(Loading)
@@ -65,18 +35,16 @@ class MarvelRepositoryImpl @Inject constructor(
         }
     }
 
-
     override suspend fun searchCharacters(characterName: String): List<Character> {
         val dbResult = characterDao.searchCharacters("%${characterName}%")
             .map { characterMapper.mapEntityToDomain(it) }
         return if (dbResult.isEmpty()) {
-            saveSearchCharacters(characterName)!!
+            saveSearchCharacters(characterName)
         } else {
             dbResult
         }
     }
 
-    private val characterDao = MarvelDatabase.getInstanceWithoutContext().marvelCharacterDao()
     override suspend fun saveSearchCharacters(characterName: String): List<Character> {
         val characters = apiService.searchCharacters(characterName)
             .body()?.items?.results?.map { characterDto ->
@@ -92,45 +60,3 @@ class MarvelRepositoryImpl @Inject constructor(
     }
 }
 
-
-//override fun searchCharactersX(characterName: String): Flow<Resources<List<Character>?>> {
-//    return flow {
-//        emit(Loading)
-//        try {
-//            val characters = apiService.searchCharacters(characterName)
-//                .body()?.items?.results?.map { characterDto ->
-//                    characterMapper.mapRemoteToEntity(characterDto)
-//                }
-//            characters?.let { characterDao.addCharacters(it) }
-////                emit(Success(characters))
-//
-//        } catch (throwable: Throwable) {
-//            emit(Error(throwable))
-//            Log.v("ALI", throwable.message.toString())
-//        }
-//    }
-//}
-
-//    override fun getCharacter() = characterDao.getCharacters()
-
-
-//    override suspend fun refreshCharacters() {
-//        try {
-//            val response = apiService.getCharecters()
-//            val items = response.body()?.data?.items?.map {
-//                it.apply {
-//                    CharacterEntity(
-//                        id = id?.toLong() ?: 0L,
-//                        name = name ?: "",
-//                        description = description ?: "",
-//                        modified = modified ?: "",
-//                        imageUrl = "${thumbnail!!.path}.${thumbnail!!.extension}"
-//                    )
-//                }
-//            }
-//            items?.let { characterDao.addCharacters(it) }
-//
-//        } catch (e: java.lang.Exception) {
-//
-//        }
-//    }
