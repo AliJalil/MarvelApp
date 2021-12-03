9ea32be105729d5bab96b63a3e669cefd07ffecb
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-03 12:47:11 +0300

    refactor HomeViewModel

--- app/src/main/java/com/example/marvelapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/marvelapp/ui/home/HomeViewModel.kt
@@ -1,32 +1,49 @@
 package com.example.marvelapp.ui.home
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import android.util.Log
+import androidx.lifecycle.*
 import com.example.marvelapp.domain.HomeItem
 import com.example.marvelapp.domain.MarvelRepository
 import com.example.marvelapp.domain.models.Character
 import dagger.hilt.android.lifecycle.HiltViewModel
 import  com.example.marvelapp.domain.HomeItemType
+import com.example.marvelapp.ui.base.BaseViewModel
+import com.example.marvelapp.util.Resources
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.toList
 
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val repository : MarvelRepository
-): ViewModel(),CharacterInteractionListener {
+    private val repository: MarvelRepository
+) : BaseViewModel(), CharacterInteractionListener {
 
-    val charecters  = repository.getCharacters()
-    val itemsList: MutableList<HomeItem<Any>> = mutableListOf()
+    var charecters: Flow<Resources<List<Character>?>> = repository.getCharacters()
+//    val itemsList: MutableList<HomeItem<Any>> = mutableListOf()
+    var items: List<Character>? = mutableListOf()
 
-   init {
 
-       itemsList.add(HomeItem(charecters.asLiveData(), HomeItemType.TYPE_PARENT))
-//       itemsList.add(HomeItem("Hello", HomeItemType.TYPE_CHILD))
-   }
+   var itemsList = MutableLiveData<MutableList<HomeItem<Any>>>()
+
+
+    init {
+        charecters = repository.getCharacters()
+
+        viewModelScope.launch {
+             items = charecters.first { it is Resources.Success }.data
+            Log.v("ALLLI items", items.toString())
+
+            itemsList.value?.add(HomeItem(items.orEmpty(), HomeItemType.TYPE_PARENT))
+            Log.v("ALLLI", itemsList.toString())
+
+            itemsList.notifyObserver()
+        }
+
+
+
+    }
+
     override fun onClickCharacter(character: Character) {
 
     }
@@ -42,3 +59,8 @@ class HomeViewModel @Inject constructor(
 
 suspend fun <T> Flow<List<T>>.flattenToList() =
     flatMapConcat { it.asFlow() }.toList()
+
+
+fun <T> MutableLiveData<T>.notifyObserver() {
+    this.value = this.value
+}
