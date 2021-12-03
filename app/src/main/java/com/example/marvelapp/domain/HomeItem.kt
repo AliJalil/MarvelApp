1c2d3c51084c75f986c20b807a21d4a0721dd0da
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-03 12:46:58 +0300

    refactor HomeItem

--- app/src/main/java/com/example/marvelapp/domain/HomeItem.kt
+++ app/src/main/java/com/example/marvelapp/domain/HomeItem.kt
@@ -1,6 +1,13 @@
 package com.example.marvelapp.domain
 
+import com.example.marvelapp.util.Resources
+
 data class HomeItem<T>(
-    val item: T,
+    val myItem: List<T>,
+    val type: HomeItemType,
+)
+
+data class HomeItemx<T>(
+    val myItem: T,
     val type: HomeItemType,
 )
