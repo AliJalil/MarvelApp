a8441b123004b487a1e5cde4a305448dc6955873
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-03 23:35:26 +0300

    inject MarvelDatabse

--- app/src/main/java/com/example/marvelapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/marvelapp/di/RepositoryModule.kt
@@ -1,5 +1,7 @@
 package com.example.marvelapp.di
 
+import com.example.marvelapp.data.local.MarvelDatabase
+import com.example.marvelapp.data.local.daos.MarvelCharacterDao
 import com.example.marvelapp.data.remote.network.MarvelApiServices
 import com.example.marvelapp.domain.MarvelRepository
 import com.example.marvelapp.domain.MarvelRepositoryImpl
@@ -14,11 +16,23 @@ import dagger.hilt.components.SingletonComponent
 object RepositoryModule {
 
     @Provides
-    fun provideRepository(apiServices: MarvelApiServices,characterMapper: CharacterMapper): MarvelRepository =
-        MarvelRepositoryImpl(apiService = apiServices,characterMapper)
+    fun provideRepository(
+        apiServices: MarvelApiServices,
+        characterMapper: CharacterMapper,
+        marvelDatabase: MarvelDatabase
+    ): MarvelRepository =
+        MarvelRepositoryImpl(
+            apiService = apiServices,
+            characterMapper = characterMapper,
+            marvelDatabase = marvelDatabase
+        )
 
 
     @Provides
-    fun provideCharacterMapper() : CharacterMapper = CharacterMapper()
+    fun provideCharacterMapper(): CharacterMapper = CharacterMapper()
+
+    @Provides
+    fun provideMarvelDatabase(): MarvelDatabase = MarvelDatabase.getInstanceWithoutContext()
+
 
 }
