0d8eaaa58ae3d62ffa0c2ebf6fa78fd14cc65f9d
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-03 23:16:04 +0300

    refactor MarvelRepository

--- app/src/main/java/com/example/marvelapp/domain/MarvelRepository.kt
+++ app/src/main/java/com/example/marvelapp/domain/MarvelRepository.kt
@@ -9,6 +9,11 @@ import kotlinx.coroutines.flow.Flow
 interface MarvelRepository {
 
      fun getCharacters() : Flow<Resources<List<Character>?>>
-     fun searchCharacters(characterName: String) : Flow<Resources<List<Character>?>>
+     suspend fun  searchCharacters(characterName: String) : List<Character>
+
+//     fun searchCharactersX(characterName: String) : Flow<Resources<List<Character>?>>
+
+     suspend fun  saveSearchCharacters(characterName: String): List<Character>
+
 //    suspend fun refreshCharacters()
 }
