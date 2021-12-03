1e66202faddbcedd452a484e0d19d5f5b5f2e35b
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-03 23:15:19 +0300

    refactor MainActivity

--- app/src/main/java/com/example/marvelapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.marvelapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.marvelapp.R
+import com.example.marvelapp.data.local.MarvelDatabase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MarvelDatabase.getInstance(applicationContext)
 //        (application as MarvelApplication).container
     }
 }
