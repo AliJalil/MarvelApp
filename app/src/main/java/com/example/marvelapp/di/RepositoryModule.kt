133a8faf2cfadac3fef538b2b27f9e0bc688a9a8
AliJalil93 <ali.abdulzahraaj@gmail.com>
2021-12-07 11:51:35 +0300

    refactor RepositoryModule

--- app/src/main/java/com/example/marvelapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/marvelapp/di/RepositoryModule.kt
@@ -1,11 +1,12 @@
 package com.example.marvelapp.di
 
 import com.example.marvelapp.data.local.MarvelDatabase
-import com.example.marvelapp.data.local.daos.MarvelCharacterDao
 import com.example.marvelapp.data.remote.network.MarvelApiServices
 import com.example.marvelapp.domain.MarvelRepository
 import com.example.marvelapp.domain.MarvelRepositoryImpl
-import com.example.marvelapp.domain.models.CharacterMapper
+import com.example.marvelapp.domain.models.CharacterDomainMapper
+import com.example.marvelapp.domain.models.CharacterEntityMapper
+import com.example.marvelapp.domain.models.CharacterEntityToDomainMapper
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,21 +19,28 @@ object RepositoryModule {
     @Provides
     fun provideRepository(
         apiServices: MarvelApiServices,
-        characterMapper: CharacterMapper,
+        characterDomainMapper: CharacterDomainMapper,
+        characterEntityMapper: CharacterEntityMapper,
+        characterEntityToDomainMapper: CharacterEntityToDomainMapper,
         marvelDatabase: MarvelDatabase
     ): MarvelRepository =
         MarvelRepositoryImpl(
             apiService = apiServices,
-            characterMapper = characterMapper,
+            characterDomainMapper = characterDomainMapper,
+            characterEntityMapper = characterEntityMapper,
+            characterEntityToDomainMapper = characterEntityToDomainMapper,
             marvelDatabase = marvelDatabase
         )
 
 
     @Provides
-    fun provideCharacterMapper(): CharacterMapper = CharacterMapper()
+    fun provideCharacterDomainMapper(): CharacterDomainMapper = CharacterDomainMapper()
 
-//    @Provides
-//    fun provideMarvelDatabase(): MarvelDatabase = MarvelDatabase.getInstanceWithoutContext()
-//
+    @Provides
+    fun provideCharacterEntityMapper(): CharacterEntityMapper = CharacterEntityMapper()
+
+    @Provides
+    fun provideCharacterEntityToDomainMapper(): CharacterEntityToDomainMapper =
+        CharacterEntityToDomainMapper()
 
 }
